ea56ea129033be6067a5b178986055391246a7b8
satapathytanmay <satapathyt8@Gmail.com>
2025-12-02 13:02:35 +0530

    Update user.java

--- user.java
+++ user.java
@@ -3,11 +3,9 @@ public static void main(system.in)
 {
 int i=9;
 int j=8;
-<<<<<<< HEAD
 int k=4;
 int l=9;
-=======
 string u="tanmay";
 boolean=false;
->>>>>>> 5a990088694f9439a6a07b9bd5c167cde6657893
+char='c';
 }
